cdb3f0878390edef20673fd1a762bcc9c27db933
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2025-12-01 15:33:01 +0900

    Add Vulkan validation layers and debug messenger setup
    Introduces support for Vulkan validation layers and sets up a debug messenger for improved debugging. Adds instance and extension checks, and integrates a debug callback for validation messages. Removes unused <stdexcept> include.

--- Renderer/main.cpp
+++ Renderer/main.cpp
@@ -9,7 +9,6 @@ import vulkan_hpp;
 
 #include <memory>
 #include <iostream>
-#include <stdexcept>
 #include <cstdlib>
 
 using namespace std;
@@ -18,12 +17,21 @@ using namespace vk;
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
 
+const vector<char const*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
+
 class HelloTriangleApplication
 {
+	// Needs custom deleter because GLFW is a C library
 	unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)> window{ nullptr, glfwDestroyWindow };
 
 	raii::Context context{};
 	raii::Instance instance = nullptr;
+	raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
 
 	void InitWindow()
 	{
@@ -37,7 +45,8 @@ class HelloTriangleApplication
 
 	void InitVulkan()
 	{
-
+		CreateInstance();
+		SetupDebugMessenger();
 	}
 
 	void MainLoop()
@@ -48,6 +57,96 @@ class HelloTriangleApplication
 	void Cleanup()
 	{
 		glfwTerminate();
+	}
+
+	void CreateInstance()
+	{
+		constexpr ApplicationInfo appInfo
+		{
+			"Hello Triangle",
+			VK_MAKE_VERSION(1, 0, 0),
+			"No Engine",
+			VK_MAKE_VERSION(1, 0, 0),
+			ApiVersion14
+		};
+
+		vector<const char*> requiredLayers;
+		if (enableValidationLayers) requiredLayers.assign(validationLayers.begin(), validationLayers.end());
+
+		vector<LayerProperties> layerProperties = context.enumerateInstanceLayerProperties();
+		for (const char* requiredLayer : requiredLayers)
+		{
+			if (ranges::none_of(layerProperties, [requiredLayer](auto const& layerProperty) { return strcmp(layerProperty.layerName, requiredLayer) == 0; }))
+			{
+				throw runtime_error("Required layer not supported: " + string(requiredLayer));
+			}
+		}
+
+		vector<const char*> requiredExtensions = getRequiredExtensions();
+		vector<ExtensionProperties> extensionProperties = context.enumerateInstanceExtensionProperties();
+		for (const char* requiredExtension : requiredExtensions)
+		{
+			if (ranges::none_of(extensionProperties, [requiredExtension](auto const& extensionProperty) { return strcmp(extensionProperty.extensionName, requiredExtension) == 0; }))
+			{
+				throw runtime_error("Required extension not supported: " + string(requiredExtension));
+			}
+		}
+
+		InstanceCreateInfo createInfo
+		{
+			{},
+			&appInfo,
+			static_cast<uint32_t>(requiredLayers.size()),
+			requiredLayers.data(),
+			static_cast<uint32_t>(requiredExtensions.size()),
+			requiredExtensions.data()
+		};
+
+		instance = raii::Instance{ context, createInfo };
+	}
+
+	void SetupDebugMessenger()
+	{
+		if (!enableValidationLayers) return;
+
+		DebugUtilsMessageSeverityFlagsEXT severityFlags(DebugUtilsMessageSeverityFlagBitsEXT::eVerbose | DebugUtilsMessageSeverityFlagBitsEXT::eWarning | DebugUtilsMessageSeverityFlagBitsEXT::eError);
+		DebugUtilsMessageTypeFlagsEXT messageTypeFlags(DebugUtilsMessageTypeFlagBitsEXT::eGeneral | DebugUtilsMessageTypeFlagBitsEXT::ePerformance | DebugUtilsMessageTypeFlagBitsEXT::eValidation);
+		DebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfoEXT
+		{
+			{},
+			severityFlags,
+			messageTypeFlags,
+			&debugCallback
+		};
+		debugMessenger = instance.createDebugUtilsMessengerEXT(debugUtilsMessengerCreateInfoEXT);
+	}
+
+	vector<const char*> getRequiredExtensions()
+	{
+		uint32_t glfwExtensionCount = 0;
+
+		const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+		if (enableValidationLayers) extensions.push_back(EXTDebugUtilsExtensionName);
+
+		return extensions;
+	}
+
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback
+	(
+		DebugUtilsMessageSeverityFlagBitsEXT severity,
+		DebugUtilsMessageTypeFlagsEXT type,
+		const DebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* /*pUserData*/
+	)
+	{
+		if (severity == DebugUtilsMessageSeverityFlagBitsEXT::eError || severity == DebugUtilsMessageSeverityFlagBitsEXT::eWarning)
+		{
+			cerr << "validation layer: type " << to_string(type) << " msg: " << pCallbackData->pMessage << endl;
+		}
+
+		return False;
 	}
 
 public:
