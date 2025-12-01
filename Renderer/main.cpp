19461e04a20697e62099afd35de8262637893c3e
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2025-12-01 17:21:48 +0900

    Implement swap chain and image view creation
    Added methods for creating the Vulkan surface, picking a physical device, creating a logical device, swap chain, and image views. Refactored validation layer and extension retrieval, and added utility functions for swap chain configuration. These changes lay the groundwork for rendering by setting up the necessary Vulkan objects.

--- Renderer/main.cpp
+++ Renderer/main.cpp
@@ -17,7 +17,7 @@ using namespace vk;
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
 
-const vector<char const*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
+const vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
 #ifdef NDEBUG
 constexpr bool enableValidationLayers = false;
 #else
@@ -32,7 +32,16 @@ class HelloTriangleApplication
 	raii::Context context{};
 	raii::Instance instance = nullptr;
 	raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
+	raii::SurfaceKHR surface = nullptr;
 	raii::PhysicalDevice physicalDevice = nullptr;
+	raii::Device device = nullptr;
+	raii::Queue graphicsQueue = nullptr;
+	raii::SwapchainKHR swapChain = nullptr;
+
+	vector<Image> swapChainImages;
+	SurfaceFormatKHR swapChainSurfaceFormat{};
+	Extent2D swapChainExtent{};
+	vector<raii::ImageView> swapChainImageViews;
 
 	vector<const char*> requiredDeviceExtension =
 	{
@@ -56,6 +65,12 @@ class HelloTriangleApplication
 	{
 		CreateInstance();
 		SetupDebugMessenger();
+		CreateSurface();
+		PickPhysicalDevice();
+		CreateLogicalDevice();
+		CreateSwapChain();
+		CreateImageViews();
+		CreateGraphicsPipeline();
 	}
 
 	void MainLoop()
@@ -91,7 +106,7 @@ class HelloTriangleApplication
 			}
 		}
 
-		vector<const char*> requiredExtensions = getRequiredExtensions();
+		vector<const char*> requiredExtensions = GetRequiredExtensions();
 		vector<ExtensionProperties> extensionProperties = context.enumerateInstanceExtensionProperties();
 		for (const char* requiredExtension : requiredExtensions)
 		{
@@ -125,9 +140,19 @@ class HelloTriangleApplication
 			{},
 			severityFlags,
 			messageTypeFlags,
-			&debugCallback
+			&DebugCallback
 		};
 		debugMessenger = instance.createDebugUtilsMessengerEXT(debugUtilsMessengerCreateInfoEXT);
+	}
+
+	void CreateSurface()
+	{
+		VkSurfaceKHR _surface = nullptr;
+		if (glfwCreateWindowSurface(static_cast<VkInstance>(*instance), window.get(), nullptr, &_surface) != VK_SUCCESS)
+		{
+			throw runtime_error("failed to create window surface!");
+		}
+		surface = raii::SurfaceKHR{ instance, _surface };
 	}
 
 	void PickPhysicalDevice()
@@ -144,23 +169,165 @@ class HelloTriangleApplication
 				bool supportsGraphics = ranges::any_of(queueFamilies, [](const QueueFamilyProperties& qfp) { return !!(qfp.queueFlags & QueueFlagBits::eGraphics); });
 
 				vector<ExtensionProperties> availableDeviceExtensions = device.enumerateDeviceExtensionProperties();
-				bool supportsAllRequiredExtensions = ranges::all_of(requiredDeviceExtension, [&availableDeviceExtensions](const char* requiredDeviceExtension)
+				bool supportsAllRequiredExtensions = ranges::all_of
+				(
+					requiredDeviceExtension, [&availableDeviceExtensions](const char* requiredDeviceExtension)
 					{
-					return ranges::any_of(availableDeviceExtensions, [requiredDeviceExtension](const ExtensionProperties& availableDeviceExtension)
+					return ranges::any_of
+					(
+						availableDeviceExtensions, [requiredDeviceExtension](const ExtensionProperties& availableDeviceExtension)
 						{
 							return strcmp(availableDeviceExtension.extensionName, requiredDeviceExtension) == 0;
-						});
-					});
+						}
+					);
+					}
+				);
 
 				auto features = device.template getFeatures2<PhysicalDeviceFeatures2, PhysicalDeviceVulkan13Features, PhysicalDeviceExtendedDynamicStateFeaturesEXT>();
 				bool supportsRequiredFeatures = features.template get<PhysicalDeviceVulkan13Features>().dynamicRendering &&
 					features.template get<PhysicalDeviceExtendedDynamicStateFeaturesEXT>().extendedDynamicState;
 
 				return supportsVulkan1_3 && supportsGraphics && supportsAllRequiredExtensions && supportsRequiredFeatures;
-			});
+			}
+		);
+		if (devIter != devices.end()) physicalDevice = *devIter;
+		else throw runtime_error("failed to find a suitable GPU!");
 	}
 
-	vector<const char*> getRequiredExtensions()
+	void CreateLogicalDevice()
+	{
+		vector<QueueFamilyProperties> queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
+		auto graphicsQueueFamilyProperty = ranges::find_if(queueFamilyProperties, [](const QueueFamilyProperties& qfp) { return (qfp.queueFlags & QueueFlagBits::eGraphics) != static_cast<QueueFlags>(0); });
+		assert(graphicsQueueFamilyProperty != queueFamilyProperties.end() && "No graphics queue family found!");
+
+		uint32_t graphicsIndex = static_cast<uint32_t>(distance(queueFamilyProperties.begin(), graphicsQueueFamilyProperty));
+
+		PhysicalDeviceFeatures2 featureChain = {};
+		PhysicalDeviceVulkan13Features vulkan13Features = {};
+		vulkan13Features.dynamicRendering = true;
+		PhysicalDeviceExtendedDynamicStateFeaturesEXT extendedDynamicStateFeatures = {};
+		extendedDynamicStateFeatures.extendedDynamicState = true;
+
+		StructureChain<PhysicalDeviceFeatures2, PhysicalDeviceVulkan13Features, PhysicalDeviceExtendedDynamicStateFeaturesEXT> featureStructureChain
+		{
+			featureChain,
+			vulkan13Features,
+			extendedDynamicStateFeatures
+		};
+
+		constexpr float queuePriority = 0.5f;
+		DeviceQueueCreateInfo queueCreateInfo
+		{
+			{},
+			graphicsIndex,
+			1,
+			&queuePriority
+		};
+
+		DeviceCreateInfo createInfo = {};
+		createInfo.pNext = &featureStructureChain.get<PhysicalDeviceFeatures2>();
+		createInfo.queueCreateInfoCount = 1;
+		createInfo.pQueueCreateInfos = &queueCreateInfo;
+		createInfo.enabledExtensionCount = static_cast<uint32_t>(requiredDeviceExtension.size());
+		createInfo.ppEnabledExtensionNames = requiredDeviceExtension.data();
+
+		device = raii::Device{ physicalDevice, createInfo };
+		graphicsQueue = raii::Queue{ device, graphicsIndex, 0 };
+	}
+
+	void CreateSwapChain()
+	{
+		SurfaceCapabilitiesKHR surfaceCapabilities = physicalDevice.getSurfaceCapabilitiesKHR(*surface);
+		swapChainExtent = ChooseSwapExtent(surfaceCapabilities);
+		swapChainSurfaceFormat = ChooseSwapSurfaceFormat(physicalDevice.getSurfaceFormatsKHR(*surface));
+
+		SwapchainCreateInfoKHR swapChainCreateInfo
+		{
+			{},
+			*surface,
+			ChooseSwapMinImageCount(surfaceCapabilities),
+			swapChainSurfaceFormat.format,
+			swapChainSurfaceFormat.colorSpace,
+			swapChainExtent,
+			1,
+			ImageUsageFlagBits::eColorAttachment,
+			SharingMode::eExclusive,
+			0,
+			nullptr,
+			surfaceCapabilities.currentTransform,
+			CompositeAlphaFlagBitsKHR::eOpaque,
+			ChooseSwapPresentMode(physicalDevice.getSurfacePresentModesKHR(*surface)),
+			true
+		};
+
+		swapChain = raii::SwapchainKHR{ device, swapChainCreateInfo };
+		swapChainImages = swapChain.getImages();
+	}
+
+	void CreateImageViews()
+	{
+		assert(swapChainImageViews.empty());
+		ImageViewCreateInfo imageViewCreateInfo
+		{
+			{},
+			{},
+			ImageViewType::e2D,
+			swapChainSurfaceFormat.format,
+			ComponentMapping{},
+			ImageSubresourceRange{ ImageAspectFlagBits::eColor, 0, 1, 0, 1 }
+		};
+
+		for (const auto& image : swapChainImages)
+		{
+			imageViewCreateInfo.image = image;
+			swapChainImageViews.emplace_back(device, imageViewCreateInfo);
+		}
+	}
+
+	void CreateGraphicsPipeline()
+	{
+
+	}
+
+	static uint32_t ChooseSwapMinImageCount(SurfaceCapabilitiesKHR const& surfaceCapabilities)
+	{
+		uint32_t minImageCount = max(3u, surfaceCapabilities.minImageCount);
+		if (surfaceCapabilities.maxImageCount && (surfaceCapabilities.maxImageCount < minImageCount)) minImageCount = surfaceCapabilities.maxImageCount;
+
+		return minImageCount;
+	}
+
+	static SurfaceFormatKHR ChooseSwapSurfaceFormat(vector<SurfaceFormatKHR> const& availableFormats)
+	{
+		assert(!availableFormats.empty());
+		const auto formatIt = ranges::find_if(availableFormats, [](const auto& format) { return format.format == Format::eB8G8R8A8Srgb && format.colorSpace == ColorSpaceKHR::eSrgbNonlinear; });
+
+		return formatIt != availableFormats.end() ? *formatIt : availableFormats[0];
+	}
+
+	static PresentModeKHR ChooseSwapPresentMode(const vector<PresentModeKHR>& availablePresentModes)
+	{
+		assert(ranges::any_of(availablePresentModes, [](auto presentMode) { return presentMode == PresentModeKHR::eFifo; }));
+
+		return ranges::any_of(availablePresentModes, [](const PresentModeKHR value) { return PresentModeKHR::eMailbox == value; }) ? PresentModeKHR::eMailbox : PresentModeKHR::eFifo;
+	}
+
+	Extent2D ChooseSwapExtent(const SurfaceCapabilitiesKHR& capabilities)
+	{
+		if (capabilities.currentExtent.width != 0xFFFFFFFF) return capabilities.currentExtent;
+
+		int width = 0;
+		int height = 0;
+		glfwGetFramebufferSize(window.get(), &width, &height);
+
+		return
+		{
+			clamp<uint32_t>(static_cast<uint32_t>(width), capabilities.minImageExtent.width, capabilities.maxImageExtent.width),
+			clamp<uint32_t>(static_cast<uint32_t>(height), capabilities.minImageExtent.height, capabilities.maxImageExtent.height)
+		};
+	}
+
+	vector<const char*> GetRequiredExtensions()
 	{
 		uint32_t glfwExtensionCount = 0;
 
@@ -172,7 +339,7 @@ class HelloTriangleApplication
 		return extensions;
 	}
 
-	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback
+	static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback
 	(
 		DebugUtilsMessageSeverityFlagBitsEXT severity,
 		DebugUtilsMessageTypeFlagsEXT type,
